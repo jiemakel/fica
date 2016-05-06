798a41a4d7c7a6740e8b3c13412b6a441bcd381f
Eetu Mäkelä <eetu.makela@aalto.fi>
2016-05-06 08:49:49 +0300

    update build scripts

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,6 +1,6 @@
 namespace app {
   'use strict'
-  let m: angular.IModule = angular.module('app', [ 'http-auth-interceptor', 'ui.router' ])
+  let m: angular.IModule = angular.module('app', [ 'http-auth-interceptor', 'ui.router', 'ngFileUpload', 'ui.bootstrap', 'ui.bootstrap.tpls' ])
   m.config(($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) => {
     $urlRouterProvider.otherwise('/')
     $stateProvider.state('main', {
--- app/scripts/main-ctrl.ts
+++ app/scripts/main-ctrl.ts
@@ -6,6 +6,7 @@ namespace app {
 
   export class MainController {
     constructor(private $scope: IMainScope) {
+
     }
   }
 }
