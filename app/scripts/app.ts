950af30f53e3e8937fe0a2e034a1991f32858ff6
Eetu Mäkelä <eetu.makela@iki.fi>
2016-05-12 12:24:19 +0300

    progress toward first iteration

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,6 +1,6 @@
 namespace app {
   'use strict'
-  let m: angular.IModule = angular.module('app', [ 'http-auth-interceptor', 'ui.router', 'ngFileUpload', 'ui.bootstrap', 'ui.bootstrap.tpls' ])
+  let m: angular.IModule = angular.module('app', [ 'http-auth-interceptor', 'ui.router', 'ngFileUpload', 'ui.bootstrap', 'ui.bootstrap.tpls', 'ngStorage' ])
   m.config(($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) => {
     $urlRouterProvider.otherwise('/')
     $stateProvider.state('main', {
